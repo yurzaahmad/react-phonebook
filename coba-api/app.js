90be392877f0ab5ddcd9965754b270225bea0052
yurza <ahmadyurza@gmail.com>
2020-09-12 22:33:52 +0700

    update

--- coba-api/app.js
+++ coba-api/app.js
@@ -2,9 +2,16 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors')
+
+mongoose.connect('mongodb://localhost:27017/chatdb', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log(`You're connected to database server`))
+    .catch((err) => console.error(err));
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var chatsRouter = require('./routes/chats');
 
 var app = express();
 
@@ -13,8 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(cors())
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/chats', chatsRouter);
 
 module.exports = app;
--- coba-react/src/index.js
+++ coba-react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoBox from './component/TodoBox';
+import TodoBox from './components/TodoBox';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
